08c6eeed5ab046393ee3160af8de6d7c3393042f
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-05-27 17:35:23 -0300

    Main Activity

---
+++ app/src/main/java/com/devbrunini/motivation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.devbrunini.motivation
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devbrunini.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
+        if (v.id == R.id.button_new_phrase){
+            
+        }
+
+    }
+}
