a2c58dc4d8c02508372b659bca03f194ad56fad8
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-05-27 19:36:37 -0300

    User Activity

--- app/src/main/java/com/devbrunini/motivation/UserActivity.kt
+++ app/src/main/java/com/devbrunini/motivation/UserActivity.kt
@@ -1,38 +1,53 @@
 package com.devbrunini.motivation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.devbrunini.motivation.databinding.ActivityMainBinding
+import com.devbrunini.motivation.databinding.ActivityUserBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class UserActivity : AppCompatActivity(), OnClickListener {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityUserBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityUserBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.buttonNewPhrase.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View) {
 
-        if (v.id == R.id.button_new_phrase){
-
+        if (v.id == R.id.button_save) {
+            handleSave()
         }
-
     }
+
+    private fun handleSave() {
+        val name = binding.textName.text.toString()
+        if (name != "") {
+            startActivity(
+                Intent(this, MainActivity::class.java))
+                finish()
+        } else {
+            Toast.makeText(this, "Preencha seu nome", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
